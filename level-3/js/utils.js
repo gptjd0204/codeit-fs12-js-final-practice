20242b60aec41704b3310477edd110913d28ce92
gptjd0204 <gptjd0204@naver.com>
2026-03-16 00:11:33 +0900

    refactor: 옵셔널체이닝 추가

--- level-3/js/utils.js
+++ level-3/js/utils.js
@@ -4,11 +4,6 @@ import { getExpenseList } from "./api.js";
 // 최신순 / 오래된순 / 높은금액순 / 낮은금액순 정렬
 export async function getSortList(category, type) {
   const expenseList = await getExpenseList();
-  if (!expenseList) {
-    console.error("목록 조회에 실패했습니다!");
-    return;
-  }
-
   const copiedExpenseList = [...expenseList];
   let categoryFilterList = copiedExpenseList;
 
@@ -17,7 +12,7 @@ export async function getSortList(category, type) {
     console.log(`카테고리 "전체" 필터 성공 => `, categoryFilterList);
   } else {
     categoryFilterList = copiedExpenseList.filter((expense) => {
-      return category === expense.category;
+      return category === expense?.category;
     });
     console.log(`카테고리 "${category}" 필터 성공 => `, categoryFilterList);
   }
@@ -26,7 +21,7 @@ export async function getSortList(category, type) {
   if (type === "recent") {
     const sortList = categoryFilterList
       .sort(function (a, b) {
-        return new Date(a.date) - new Date(b.date);
+        return new Date(a?.date) - new Date(b?.date);
       })
       .reverse();
 
@@ -34,18 +29,18 @@ export async function getSortList(category, type) {
     return sortList;
   } else if (type === "old") {
     const sortList = categoryFilterList.sort(function (a, b) {
-      return new Date(a.date) - new Date(b.date);
+      return new Date(a?.date) - new Date(b?.date);
     });
 
     console.log("오래된순으로 정렬 => ", sortList);
     return sortList;
   } else if (type === "amountDesc") {
-    const sortList = categoryFilterList.sort((a, b) => b.amount - a.amount);
+    const sortList = categoryFilterList.sort((a, b) => b?.amount - a?.amount);
 
     console.log("높은 금액순으로 정렬 => ", sortList);
     return sortList;
   } else if (type === "amountAsc") {
-    const sortList = categoryFilterList.sort((a, b) => a.amount - b.amount);
+    const sortList = categoryFilterList.sort((a, b) => a?.amount - b?.amount);
 
     console.log("낮은 금액순으로 정렬 => ", sortList);
     return sortList;
@@ -66,7 +61,7 @@ export async function getTotalAmount() {
   // 지출 내역을 추가할 때 입력한 금액은 문자열로 저장되어 있기 때문에
   // Number()를 활용해야 정상적인 값이 나온다.
   const total = expenseList.reduce((sum, cur) => {
-    return sum + Number(cur.amount);
+    return sum + Number(cur?.amount);
   }, 0);
 
   console.log("지출 총합 => ", total);
@@ -81,11 +76,11 @@ export async function getCategoryAmount() {
   let allCategoryAmount;
   categories.forEach((category) => {
     const categoryFilter = expenseList.filter(
-      (expense) => category === expense.category,
+      (expense) => category === expense?.category,
     );
 
     const categoryTotal = categoryFilter.reduce((sum, cur) => {
-      return sum + Number(cur.amount);
+      return sum + Number(cur?.amount);
     }, 0);
 
     // []를 사용하면 객체의 key를 인자값으로 설정 가능 (대괄호 표기법)
@@ -118,7 +113,7 @@ export async function getCategoryAvgAmount(category) {
   const categoryAmount = await getCategoryAmount();
 
   const categoryFilter = expenseList.filter(
-    (expense) => category === expense.category,
+    (expense) => category === expense?.category,
   );
 
   const categoryTotal = categoryAmount[category];
