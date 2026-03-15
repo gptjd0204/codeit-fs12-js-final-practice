f1efb5af113576babd99c0abe2e21c54e9eb4794
gptjd0204 <gptjd0204@naver.com>
2026-03-16 00:00:45 +0900

    design: 전체 페이지 레이아웃 스타일 수정

--- level-3/js/render/list.js
+++ level-3/js/render/list.js
@@ -14,17 +14,29 @@ export async function renderExpenseList(sort) {
     const { id, ...expenseData } = expense;
     const editBtn = document.createElement("button");
     const deleteBtn = document.createElement("button");
-    editBtn.textContent = "수정";
-    deleteBtn.textContent = "삭제";
+    const editIcon = document.createElement("img");
+    const deleteIcon = document.createElement("img");
+    editIcon.src = "./img/edit-icon.png";
+    editIcon.alt = "수정 아이콘";
+    editIcon.classList.add("btn-icon");
+    deleteIcon.src = "./img/delete-icon.png";
+    deleteIcon.alt = "삭제 아이콘";
+    deleteIcon.classList.add("btn-icon");
+    editBtn.appendChild(editIcon);
+    deleteBtn.appendChild(deleteIcon);
+    // editBtn.textContent = "수정";
+    // deleteBtn.textContent = "삭제";
+    editBtn.classList.add("edit-btn");
+    deleteBtn.classList.add("edit-btn");
     // 지출 내역 나열
     for (const data in expenseData) {
-      const dataTh = document.createElement("th");
-      dataTh.textContent = expenseData[data];
-      tr.appendChild(dataTh);
+      const dataTd = document.createElement("td");
+      dataTd.textContent = expenseData[data];
+      tr.appendChild(dataTd);
     }
 
-    const editTh = document.createElement("th");
-    const deleteTh = document.createElement("th");
+    const editTd = document.createElement("td");
+
     // 수정 버튼 이벤트 리스너
     editBtn.addEventListener("click", function (e) {
       e.preventDefault();
@@ -44,11 +56,11 @@ export async function renderExpenseList(sort) {
       e.preventDefault();
       deleteExpense(id);
     });
-    editTh.appendChild(editBtn);
-    deleteTh.appendChild(deleteBtn);
 
-    tr.appendChild(editTh);
-    tr.appendChild(deleteTh);
+    editTd.appendChild(editBtn);
+    editTd.appendChild(deleteBtn);
+
+    tr.appendChild(editTd);
 
     expensesListEl.appendChild(tr);
   });
